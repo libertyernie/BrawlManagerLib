fcbc484b8f17e9f4d5955308918090efb6b887d0
libertyernie <libertyernie0@gmail.com>
2014-01-03 20:26:32 -0600

    Initial commit/upload

---
+++ AboutBSM.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace BrawlManagerLib {
+	partial class AboutBSM {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.software_title = new System.Windows.Forms.Label();
+			this.version = new System.Windows.Forms.Label();
+			this.copyright = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.brawllib = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// software_title
+			// 
+			this.software_title.AutoSize = true;
+			this.software_title.Dock = System.Windows.Forms.DockStyle.Left;
+			this.software_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.software_title.Location = new System.Drawing.Point(0, 0);
+			this.software_title.Name = "software_title";
+			this.software_title.Size = new System.Drawing.Size(118, 20);
+			this.software_title.TabIndex = 0;
+			this.software_title.Text = "software_title";
+			// 
+			// version
+			// 
+			this.version.AutoSize = true;
+			this.version.Dock = System.Windows.Forms.DockStyle.Right;
+			this.version.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.version.Location = new System.Drawing.Point(326, 0);
+			this.version.Name = "version";
+			this.version.Size = new System.Drawing.Size(66, 20);
+			this.version.TabIndex = 1;
+			this.version.Text = "version";
+			this.version.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// copyright
+			// 
+			this.copyright.AutoSize = true;
+			this.copyright.Dock = System.Windows.Forms.DockStyle.Top;
+			this.copyright.Location = new System.Drawing.Point(0, 0);
+			this.copyright.Name = "copyright";
+			this.copyright.Padding = new System.Windows.Forms.Padding(0, 0, 0, 4);
+			this.copyright.Size = new System.Drawing.Size(50, 17);
+			this.copyright.TabIndex = 3;
+			this.copyright.Text = "copyright";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.textBox1.Location = new System.Drawing.Point(0, 17);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox1.Size = new System.Drawing.Size(392, 156);
+			this.textBox1.TabIndex = 4;
+			// 
+			// brawllib
+			// 
+			this.brawllib.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.brawllib.Location = new System.Drawing.Point(0, 173);
+			this.brawllib.Name = "brawllib";
+			this.brawllib.Size = new System.Drawing.Size(392, 32);
+			this.brawllib.TabIndex = 5;
+			this.brawllib.Text = "brawllib";
+			this.brawllib.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.version);
+			this.panel1.Controls.Add(this.software_title);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(392, 28);
+			this.panel1.TabIndex = 5;
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.textBox1);
+			this.panel2.Controls.Add(this.brawllib);
+			this.panel2.Controls.Add(this.copyright);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel2.Location = new System.Drawing.Point(0, 28);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(392, 205);
+			this.panel2.TabIndex = 6;
+			// 
+			// AboutBSM
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(392, 233);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Name = "AboutBSM";
+			this.Text = "About";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label software_title;
+		private System.Windows.Forms.Label version;
+		private System.Windows.Forms.Label copyright;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label brawllib;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+	}
+}
---
+++ TextureContainer.cs
@@ -0,0 +1,67 @@
+﻿using BrawlLib.SSBB.ResourceNodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BrawlManagerLib {
+	public class TextureContainer : IEnumerable<TEX0Node> {
+		public TEX0Node prevbase_tex0;
+		public TEX0Node icon_tex0;
+		public TEX0Node frontstname_tex0;
+		public TEX0Node seriesicon_tex0;
+		public TEX0Node selmap_mark_tex0;
+
+		public PAT0TextureEntryNode prevbase_pat0;
+		public PAT0TextureEntryNode icon_pat0;
+		public PAT0TextureEntryNode frontstname_pat0;
+		public PAT0TextureEntryNode seriesicon_pat0;
+		public PAT0TextureEntryNode selmap_mark_pat0;
+
+		public ResourceNode TEX0Folder {get; private set;}
+		private ResourceNode PAT0Folder;
+		private int iconNum;
+
+		public IEnumerator<TEX0Node> GetEnumerator() {
+			return new List<TEX0Node> { prevbase_tex0, icon_tex0, frontstname_tex0, seriesicon_tex0, selmap_mark_tex0 }.GetEnumerator();
+		}
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
+			return GetEnumerator();
+		}
+
+		public TextureContainer() {}
+
+		/// <summary>
+		/// Finds the textures through their PAT0 entries, given an icon number and a ResourceNode
+		/// </summary>
+		/// <param name="sc_selmap">The sc_selmap *or* MiscData[80] node.</param>
+		/// <param name="iconNum">The icon index (also used in the third part of the Custom SSS code.)</param>
+		public TextureContainer(ResourceNode node, int iconNum) {
+			TEX0Folder = node.FindChild("MiscData[80]/Textures(NW4R)", false)
+					  ?? node.FindChild("Textures(NW4R)", false);
+			PAT0Folder = node.FindChild("MiscData[80]/AnmTexPat(NW4R)", false)
+					  ?? node.FindChild("AnmTexPat(NW4R)", false);
+			this.iconNum = iconNum;
+
+			populate(out prevbase_tex0, out prevbase_pat0, "MenSelmapPreview/basebgM");
+			populate(out icon_tex0, out icon_pat0, "MenSelmapIcon/iconM");
+			populate(out frontstname_tex0, out frontstname_pat0, "MenSelmapPreview/pasted__stnameM"); // the name shadow has a separate pat0 list
+			populate(out seriesicon_tex0, out seriesicon_pat0, "MenSelmapPreview/lambert113");
+			populate(out selmap_mark_tex0, out selmap_mark_pat0, "MenSelmapPreview/pasted__stnamelogoM");
+		}
+
+		private void populate(out TEX0Node tex0node, out PAT0TextureEntryNode pat0node, string path) {
+			var query = (from n in PAT0Folder.FindChild(path, false).Children[0].Children
+						 where n is PAT0TextureEntryNode
+						 && ((PAT0TextureEntryNode)n).FrameIndex == iconNum
+						 select ((PAT0TextureEntryNode)n));
+			if (!query.Any()) {
+				pat0node = null;
+				tex0node = null;
+			} else {
+				pat0node = query.First();
+				tex0node = TEX0Folder.FindChild(pat0node.Name, false) as TEX0Node;
+			}
+		}
+	}
+}
