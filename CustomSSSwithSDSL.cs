e01ef14002476b7a646c3bb5da3daee1be6f1f9b
libertyernie <libertyernie0@gmail.com>
2014-01-07 19:54:05 -0500

    Add new class - CustomSSSwithSDSL - ideally, you won't need to access CustomSSS or SDSLScanner directly anymore

---
+++ CustomSSSwithSDSL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BrawlManagerLib {
+	public class CustomSSSwithSDSL : CustomSSS {
+		public CustomSSSwithSDSL(string[] s) : base(s) {
+			SongsByStage = SDSLScanner.SongsByStage(this);
+		}
+		public CustomSSSwithSDSL(byte[] data) : base(data) {
+			SongsByStage = SDSLScanner.SongsByStage(this);
+		}
+
+		public Dictionary<byte, Song> SongsByStage { get; private set; }
+	}
+}
