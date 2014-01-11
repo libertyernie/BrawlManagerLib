ebe18a8539a8ac9abd0b5b8c1c133b191a1a7179
libertyernie <libertyernie0@gmail.com>
2014-01-10 20:13:17 -0500

    Move SongNameBar and SongPanel to library

---
+++ Songs/SongPanel.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace BrawlManagerLib {
+	partial class SongPanel {
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.grid = new System.Windows.Forms.PropertyGrid();
+			this.app = new System.Windows.Forms.AudioPlaybackPanel();
+			this.songNameBar = new SongNameBar();
+			this.SuspendLayout();
+			// 
+			// grid
+			// 
+			this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.grid.HelpVisible = false;
+			this.grid.Location = new System.Drawing.Point(0, 20);
+			this.grid.Name = "grid";
+			this.grid.Size = new System.Drawing.Size(300, 169);
+			this.grid.TabIndex = 0;
+			// 
+			// app
+			// 
+			this.app.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.app.Location = new System.Drawing.Point(0, 189);
+			this.app.Name = "app";
+			this.app.Size = new System.Drawing.Size(300, 111);
+			this.app.TabIndex = 1;
+			this.app.TargetStreams = null;
+			// 
+			// songNameBar
+			// 
+			this.songNameBar.Dock = System.Windows.Forms.DockStyle.Top;
+			this.songNameBar.Index = 0;
+			this.songNameBar.Location = new System.Drawing.Point(0, 0);
+			this.songNameBar.Name = "songNameBar";
+			this.songNameBar.Size = new System.Drawing.Size(300, 20);
+			this.songNameBar.TabIndex = 2;
+			// 
+			// SongPanel
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.grid);
+			this.Controls.Add(this.songNameBar);
+			this.Controls.Add(this.app);
+			this.Name = "SongPanel";
+			this.Size = new System.Drawing.Size(300, 300);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PropertyGrid grid;
+		private System.Windows.Forms.AudioPlaybackPanel app;
+		private SongNameBar songNameBar;
+	}
+}
---
+++ Songs/SongPanel.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using BrawlLib.SSBB.ResourceNodes;
+using System.IO;
+using BrawlManagerLib;
+using System.Audio;
+
+namespace BrawlManagerLib {
+	public partial class SongPanel : UserControl {
+		/// <summary>
+		/// The currently opened .brstm file's root node.
+		/// </summary>
+		private ResourceNode _rootNode;
+		/// <summary>
+		/// The full path to the currently opened .brstm file.
+		/// </summary>
+		private string _rootPath;
+
+		public bool LoadNames;
+		public bool LoadBrstms;
+
+		public string RootPath {
+			get {
+				return _rootPath;
+			}
+		}
+		public bool FileOpen {
+			get {
+				return _rootPath != null;
+			}
+		}
+
+		public SongPanel() {
+			InitializeComponent();
+
+			AllowDrop = true;
+			this.DragEnter += SongPanel_DragEnter;
+			this.DragDrop += SongPanel_DragDrop;
+		}
+
+		public void Close() {
+			if (_rootNode != null) {
+				_rootNode.Dispose();
+				_rootNode = null;
+			}
+			_rootPath = null;
+
+			grid.SelectedObject = null;
+			app.TargetSource = null;
+			app.Enabled = grid.Enabled = false;
+			songNameBar.Index = -1;
+		}
+		public void Open(FileInfo fi) {
+			if (_rootNode != null) {
+				_rootNode.Dispose();
+				_rootNode = null;
+			}
+			_rootPath = fi.FullName;
+			_rootNode = NodeFactory.FromFile(null, _rootPath);
+			if (LoadNames) {
+				string filename = Path.GetFileNameWithoutExtension(_rootPath).ToUpper();
+				int index = (from s in SongIDMap.Songs
+							 where s.Filename == filename
+							 select s.InfoPacIndex ?? -1)
+							 .DefaultIfEmpty(-1).First();
+				songNameBar.Index = index;
+			} else {
+				songNameBar.Index = -1;
+			}
+			if (LoadBrstms && _rootNode is IAudioSource) {
+				grid.SelectedObject = _rootNode;
+				app.TargetSource = _rootNode as IAudioSource;
+				app.Enabled = grid.Enabled = true;
+			} else {
+				grid.SelectedObject = null;
+				app.TargetSource = null;
+				app.Enabled = grid.Enabled = false;
+			}
+		}
+
+		public void Export() {
+			using (var dialog = new SaveFileDialog()) {
+				dialog.Filter = "BRSTM stream|*.brstm";
+				dialog.DefaultExt = "brstm";
+				dialog.AddExtension = true;
+				dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+				if (dialog.ShowDialog(this) == DialogResult.OK) {
+					File.Copy(RootPath, dialog.FileName, true);
+				}
+			}
+		}
+		public void Rename() {
+			using (NameDialog nd = new NameDialog()) {
+				nd.EntryText = Path.GetFileName(RootPath);
+				if (nd.ShowDialog(this) == DialogResult.OK) {
+					if (!nd.EntryText.ToLower().EndsWith(".brstm")) {
+						nd.EntryText += ".brstm"; // Force .brstm extension so it shows up in the list
+					}
+					string from = RootPath;
+					Close();
+					FileOperations.Rename(from, System.Environment.CurrentDirectory + "\\" + nd.EntryText);
+				}
+			}
+		}
+		public void Delete() {
+			if (_rootNode != null) {
+				_rootNode.Dispose();
+				_rootNode = null;
+				FileOperations.Delete(_rootPath);
+				Close();
+			}
+		}
+
+		public string findInfoFile() {
+			return songNameBar.findInfoFile();
+		}
+		public bool IsInfoBarDirty() {
+			return songNameBar.IsDirty;
+		}
+		public void save() {
+			songNameBar.save();
+		}
+
+		public void ExportMSBin(string path) {
+			songNameBar.ExportMSBin(path);
+		}
+
+		private void SongPanel_DragEnter(object sender, DragEventArgs e) {
+			if (e.Data.GetDataPresent(DataFormats.FileDrop)) { // Must be a file
+				string[] s = (string[])e.Data.GetData(DataFormats.FileDrop);
+				if (s.Length == 1) { // Can only drag and drop one file
+					string filename = s[0].ToLower();
+					if (filename.EndsWith(".brstm") || filename.EndsWith(".wav")) {
+						e.Effect = DragDropEffects.Copy;
+					}
+				}
+			}
+		}
+
+		private void SongPanel_DragDrop(object sender, DragEventArgs e) {
+			string[] s = (string[])e.Data.GetData(DataFormats.FileDrop);
+			string filepath = s[0].ToLower();
+			if (FileOpen) {
+				if (_rootNode != null) {
+					_rootNode.Dispose(); // Close the file before overwriting it!
+					_rootNode = null;
+				}
+				copyBrstm(filepath, _rootPath);
+				Open(new FileInfo(_rootPath));
+				//refreshDirectory();
+			}
+		}
+
+		/// <summary>
+		/// This method can handle WAV files, converting them to BRSTM using BrawlLib's converter.
+		/// </summary>
+		/// <param name="src">a BRSTM or WAV file</param>
+		/// <param name="dest">the output BRSTM path</param>
+		public static void copyBrstm(string src, string dest) {
+			if (src.EndsWith(".brstm")) {
+				FileOperations.Copy(src, dest); // Use FileOperations (calls Windows shell -> asks for confirmation to overwrite)
+			} else {
+				BrstmConverterDialog bcd = new BrstmConverterDialog();
+				bcd.AudioSource = src;
+				if (bcd.ShowDialog() == DialogResult.OK) {
+					// Make a temporary node to put the data in, and export it.
+					// This avoids the need to use pointers directly.
+					RSTMNode tmpNode = new RSTMNode();
+					tmpNode.ReplaceRaw(bcd.AudioData);
+					tmpNode.Export(dest);
+					tmpNode.Dispose();
+				}
+				bcd.Dispose();
+			}
+		}
+	}
+}
